fa9643ed2817d8646c35d65478332667c3b51eb1
Ap0AqPedul1 <bastomi.azhari28@gmail.com>
2025-07-09 20:08:07 +0700

    update

---
+++ setup/install_venv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Aktifkan venv jika perlu (opsional)
+# source venv/bin/activate
+
+echo "Menginstal numpy dan sounddevice..."
+
+pip install numpy
+pip install sounddevice
+
+echo "Instalasi selesai."
